464935d86b5834735c4d26aaa09f4f9f23cb4b51
jhanisha07 <163570033+jhanisha07@users.noreply.github.com>
2024-06-30 17:17:46 +0530

    Add files via upload

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Header.css'
+
+const Header = () => {
+  
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>
+"Feast your eyes on our tantalizing menu selection. From savory delights to sweet indulgences, explore a world of flavor with just a click. Your next delicious discovery awaits – dive in now!"</p>
+            <button >View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
